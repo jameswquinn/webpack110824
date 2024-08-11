2a0485f4a7adbdeff6ddc1d834c6f87f7f9b1891
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 17:23:59 +0100

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,22 +1,21 @@
 import { h } from 'preact';
-import { getImageUrl, getSrcSet } from '../utils/imageUtils';
 
 function Image({ src, alt, className }) {
-  const webpSrcSet = getSrcSet(src, 'webp');
-  const fallbackSrcSet = getSrcSet(src, 'png');
-  const fallbackSrc = getImageUrl(src, 'png', 1200);
+  const baseName = src.split('/').pop().split('.')[0];
+  const sizes = [300, 600, 1200, 2000];
+  
+  const webpSrcSet = sizes
+    .map(size => `${require(`../assets/${baseName}-${size}.webp`)} ${size}w`)
+    .join(', ');
 
   return (
-    <picture>
-      <source srcSet={webpSrcSet} type="image/webp" />
-      <img 
-        src={fallbackSrc}
-        srcSet={fallbackSrcSet}
-        sizes="(max-width: 300px) 300px, (max-width: 600px) 600px, (max-width: 1200px) 1200px, 2000px"
-        alt={alt}
-        className={className}
-      />
-    </picture>
+    <img 
+      src={require(`../assets/${baseName}-1200.webp`)}
+      srcSet={webpSrcSet}
+      sizes="(max-width: 300px) 300px, (max-width: 600px) 600px, (max-width: 1200px) 1200px, 2000px"
+      alt={alt}
+      className={className}
+    />
   );
 }
 
