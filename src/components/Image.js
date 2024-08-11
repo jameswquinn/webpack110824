b70cb9953237ad9d8d7bce1a4290b535654560fa
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 16:35:28 +0100

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,14 @@
 import { h } from 'preact';
+import WebpDetect from 'webp-detect';
 
 function Image({ src, alt, className }) {
+  const webpSrcSet = src.srcSet.split(',').map(s => s.replace(/\.(png|jpg|jpeg)/, '.webp')).join(',');
+  
   return (
     <img 
-      src={src}
+      src={src.src}
+      srcSet={WebpDetect.isSupported ? webpSrcSet : src.srcSet}
+      sizes="(max-width: 300px) 300px, (max-width: 600px) 600px, (max-width: 1200px) 1200px, 2000px"
       alt={alt}
       className={className}
     />
