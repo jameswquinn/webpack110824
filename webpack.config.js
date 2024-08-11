328aee201c463cdcefa2be6fabef3f5119caf797
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 17:23:09 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-
-  return {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
-      publicPath: '/'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: 'babel-loader'
-        },
-        {
-          test: /\.css$/,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader',
-            'postcss-loader'
-          ],
-        },
-        {
-          test: /\.png$/,
-          include: path.resolve(__dirname, 'public'),
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: 'images',
-              },
-            },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-      isProduction && new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css',
-      }),
-    ].filter(Boolean),
-    optimization: {
-      minimizer: [
-        new TerserPlugin(),
-        new CssMinimizerPlugin(),
-      ],
-    },
-    resolve: {
-      alias: {
-        'react': 'preact/compat',
-        'react-dom': 'preact/compat',
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
       },
-      extensions: ['.js', '.jsx'],
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(webp|png|jpe?g)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  resolve: {
+    alias: {
+      'react': 'preact/compat',
+      'react-dom': 'preact/compat',
     },
-  };
+    extensions: ['.js', '.jsx'],
+  },
 };
