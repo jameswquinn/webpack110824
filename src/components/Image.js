2c37456de579aa89db785b301b2ba007e676f7bf
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 17:43:02 +0100

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,17 +1,13 @@
 import { h } from 'preact';
 
 function Image({ src, alt, className }) {
-  const baseName = src.split('/').pop().split('.')[0];
-  const sizes = [300, 600, 1200, 2000];
-  
-  const webpSrcSet = sizes
-    .map(size => `${require(`../assets/${baseName}-${size}.webp`)} ${size}w`)
-    .join(', ');
+  const webpImage = require(`../assets/${src}?sizes[]=300,sizes[]=600,sizes[]=1200,sizes[]=2000&format=webp`);
+  const fallbackImage = require(`../assets/${src}?sizes[]=300,sizes[]=600,sizes[]=1200,sizes[]=2000&format=fallback`);
 
   return (
     <img 
-      src={require(`../assets/${baseName}-1200.webp`)}
-      srcSet={webpSrcSet}
+      src={webpImage.src}
+      srcSet={webpImage.srcSet}
       sizes="(max-width: 300px) 300px, (max-width: 600px) 600px, (max-width: 1200px) 1200px, 2000px"
       alt={alt}
       className={className}
